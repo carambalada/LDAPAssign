f4a53de3433ca4ceb255fd9231610509bbac1bbd
Муссалимов Тимур <timur@xrobo.ru>
2018-02-25 23:07:58 +0300

    Printing result - no changes to LDAP

--- README.md
+++ README.md
@@ -4,14 +4,31 @@ LDAP. Placing accounts to groups based on a given text configuration
 Configuration example (config.py):
 
 ```python
-ldap_settings = {
- 'hostname': 'host',
- 'user_dn': 'cn=bind_user,ou=service,dc=company,dc=domain',
- 'password': 'pass'
+ldap = {
+ 'connection': {
+  'hostname': 'host',
+  'user_dn': 'cn=bind_user,ou=service,dc=company,dc=domain',
+  'password': 'pass'
+ },
+ 'users': {
+ 'base': 'ou=Users,dc=company,dc=domain',
+ 'scope': 1,
+ 'filter': '(&(objectClass=posixAccount)(|(uid=user*)))',
+ 'attributes': ['uid'],
+ 'attrsonly': 0
+ },
+ 'groups': {
+ 'base': 'ou=Groups,dc=company,dc=domain',
+ 'scope': 1,
+ 'filter': '(objectClass=posixGroup)',
+ 'attributes': ['memberUid'],
+ 'attrsonly': 0
+ }
 }
 
 groups = [
  { 'name': 'Group1',
+   'limit': 10,
    'patterns':
     [ 'user11',
       'user12',
@@ -19,6 +36,7 @@ groups = [
     ]
  },
  { 'name': 'Group2',
+   'limit': 20,
    'patterns':
     [ 'user21',
       'user22',
@@ -26,14 +44,16 @@ groups = [
  },
  { 'name': 'Users',
    'patterns':
+   'limit': 100,
     [ 'user',
     ]
  },
 ]
-
-users = [
- 'user11-usr',
- 'user21-usr',
- 'userXX-usr',
-]
 ```
+
+Scope in "ldap" section can be one of:
+
+* 0 (SCOPE_BASE) - to search the object itself
+* 1 (SCOPE_ONELEVEL) - to search the object's immediate children
+* 2 (SCOPE_SUBTREE) - to search the object and all its descendants
+
--- ldapassign.py
+++ ldapassign.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python
 
-import config
-import ldap
+# TODO:
+# - create groups if they don't exist
 
-def g_match(user, groups):
- group_list = []
- for group in groups:
-  for pattern in group['patterns']:
-   if user.startswith(str(pattern)):
-    group_list.append(group['name'])
-    break
- return group_list
+import ldap
+import config
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 #
-# LDAP connection
+# LDAP: Binding
 #
 def ldap_connection(hostname, username, password):
  url = "ldap://" + hostname
@@ -37,15 +31,158 @@ def ldap_connection(hostname, username, password):
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 #
+# LDAP: Getting list of users
+#
+def get_users(b, settings):
+ users = []
+ result_id = b.search(
+  settings['base'],
+  settings['scope'],
+  settings['filter'],
+  settings['attributes'],
+  settings['attrsonly']
+  )
+ 
+ result_type, result_data = b.result(result_id, 1)
+ #print(str(result_type))
+ #print(str(result_data))
+ if result_data != []:
+  for entry in result_data:
+   users.append(
+    { 'dn': entry[0],
+      'uid': entry[1]['uid'][0]
+    }
+   )
+  return users
+ else:
+  print('No configured users found in LDAP - imposible to continue')
+  exit(1)
+ 
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#
+# Getting matching groups
+#
+def get_matching_groups(user, groups):
+ groups_match = []
+ for group in groups:
+  for pattern in group['patterns']:
+   if user['uid'].startswith(pattern):
+    groups_match.append(group)
+    break
+ return groups_match
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#
+# LDAP: Making a list of groups from the config plus:
+#        - memberUid
+#        - number of members
+#
+def get_groups(bind, config_groups, settings):
+ #
+ # making a filter like '(&(objectClass=posixGroup)(cn=Group1))
+ # TODO: check that at least one group is valid for forming the filter
+ #
+ groups = []
+ for group in config_groups:
+  search_filter = '(&' + settings['filter'] + '(cn=' + group['name'] + '))'
+  result_id = bind.search(
+   settings['base'],
+   settings['scope'],
+   search_filter,
+   settings['attributes'],
+   settings['attrsonly']
+   )
+  result_type, result_data = bind.result(result_id, 1)
+  if result_data != []:
+   if result_data[0][1] != {}:
+    group['memberUid'] = result_data[0][1]['memberUid']
+    group['current'] = len(group['memberUid'])
+   else:
+    group['memberUid'] = []
+    group['current'] = 0
+   groups.append(group)
+  else:
+   print('Group ' + group['name'] + ' was not found in LDAP - skipping')
+
+ if groups != []:
+  return groups
+ else:
+  print('No configured groups found in LDAP - imposible to continue')
+  exit(1)
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#
+# Getting users that are not found in groups
+#
+def get_unassigned_users(users, groups):
+ #
+ # is it okay to use the same variable for looping and removig operation at
+ # the same time?
+ #
+ unassigned_users = users
+ for user in users:
+  for group in groups:
+   if group['memberUid'].count(user['uid']) > 0:
+    #print('User ' + user['uid'] + ' found in group ' + group['name'])
+    unassigned_users.remove(user)
+    break
+ return unassigned_users
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#
+# Getting groups that are still unfilled
+#
+def get_unfilled_groups(groups):
+ unfilled_groups = groups
+ for group in groups:
+  if group['current'] >= group['limit']:
+   unfilled_groups.remove(group)
+ return unfilled_groups
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#
+# Arranging user to groups
+#
+def arrange(users, groups):
+ failed_users = []
+ for user in users:
+  groups_match = get_matching_groups(user, groups)
+  if groups_match != []:
+   print('Adding user ' + user['uid'] + ' to the first unfilled group ' \
+          + groups[0]['name'])
+   groups[0]['current'] += 1
+   groups = get_unfilled_groups(groups)
+  else:
+   failed_users.append(user)
+ return failed_users
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#
 # Main
 #
 
-# for user in config.users:
-#  g = g_match(user,config.groups)
-#  print('User ' + str(user) + ' should be at groups ' + str(g))
-
 bind = ldap_connection(
- config.ldap_settings['hostname'],
- config.ldap_settings['user_dn'],
- config.ldap_settings['password']
+ config.ldap['connection']['hostname'],
+ config.ldap['connection']['user_dn'],
+ config.ldap['connection']['password']
  )
+
+users = get_users(bind, config.ldap['users'])
+groups = get_groups(bind, config.groups, config.ldap['groups'])
+users = get_unassigned_users(users, groups)
+groups = get_unfilled_groups(groups)
+users = arrange(users, groups)
+print(groups)
+if users != []:
+ print('Groupping some users were failed due to groups\' limitation or\
+ pattern dismatch. Here are some hints:')
+ for user in users:
+  print('\nUser ' + user['uid'] + ':')
+  groups_match = get_matching_groups(user, config.groups)
+  if groups_match == []:
+   print(' - no matching groups found')
+  else:
+   for group in groups_match:
+    print(' - matching with group ' + group['name'] + ' (limit: ' + \
+           str(group['limit']) + ', users: ' + str(group['current']) + ')')
